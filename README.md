31a0442c55211e5e00d155d9aad510596cdd6f91
Oanh Nguyen <oanhnn.bk@gmail.com>
2018-10-18 16:17:46 +0700

    The first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Introduction
+
+[![Build Status](https://travis-ci.org/oanhnn/laravel-nestable.svg?branch=master)](https://travis-ci.org/oanhnn/laravel-nestable)
+[![Coverage Status](https://coveralls.io/repos/github/oanhnn/laravel-nestable/badge.svg?branch=master)](https://coveralls.io/github/oanhnn/laravel-nestable?branch=master)
+
+Easy make nestable model in Laravel 5.5+ Application
+
+## Requirements
+
+* php >=7.1.3
+* Laravel 5.5+
+
+## TODO
+
+- [ ] Write test classes
+- [ ] Write documents
+
+## Installation
+
+Begin by pulling in the package through Composer.
+
+```bash
+$ composer require oanhnn/laravel-nestable
+```
+
+## Usage
+
+
+## Changelog
+
+See all change logs in [CHANGELOG](CHANGELOG.md)
+
+## Testing
+
+```bash
+$ git clone git@github.com/oanhnn/laravel-nestable.git /path
+$ cd /path
+$ composer install
+$ composer phpunit
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+## Security
+
+If you discover any security related issues, please email to [Oanh Nguyen](mailto:oanhnn.bk@gmail.com) instead of 
+using the issue tracker.
+
+## Credits
+
+- [Oanh Nguyen](https://github.com/oanhnn)
+- [All Contributors](../../contributors)
+
+## License
+
+This project is released under the MIT License.   
+Copyright © 2018 [Oanh Nguyen](https://oanhnn.github.io/).
---
+++ src/TreeCollection.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Laravel\Nestable;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class TreeCollection extends Collection
+{
+    /**
+     * Converts a flat collection of nested set models to an set where
+     * children is eager loaded
+     *
+     * @param bool $removeOrphans Remove nodes that exist without their parents.
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function toNested($removeOrphans = true)
+    {
+        // Set new collection for "children" relations
+        $collection = $this->getDictionary();
+
+        /**
+         * @var int $key
+         * @var \Illuminate\Database\Eloquent\Model $model
+         */
+        foreach ($collection as $key => $model) {
+            $model->setRelation('children', new Collection);
+        }
+
+        // Assign all child nodes to their parents
+        $nestedKeys = [];
+
+        /**
+         * @var int $key
+         * @var \Illuminate\Database\Eloquent\Model $model
+         */
+        foreach ($collection as $key => $model) {
+            if (!$parentKey = $model->getParentId()) {
+                continue;
+            }
+
+            if (array_key_exists($parentKey, $collection)) {
+                $collection[$parentKey]->children[] = $model;
+                $nestedKeys[] = $model->getKey();
+            } elseif ($removeOrphans) {
+                $nestedKeys[] = $model->getKey();
+            }
+        }
+
+        // Remove processed nodes
+        foreach ($nestedKeys as $key) {
+            unset($collection[$key]);
+        }
+
+        return new Collection($collection);
+    }
+
+    /**
+     * Gets an array with values of a given column. Values are indented according to their depth.
+     *
+     * @param  string $value Array values
+     * @param  string $key Array keys
+     * @param  string $indent Character to indent depth
+     * @return array
+     */
+    public function listsNested($value, $key = null, $indent = '&nbsp;&nbsp;&nbsp;')
+    {
+        // Recursive helper function
+        $buildCollection = function ($items, $depth = 0) use (&$buildCollection, $value, $key, $indent) {
+            $result = [];
+            $indentString = str_repeat($indent, $depth);
+            foreach ($items as $item) {
+                if ($key !== null) {
+                    $result[$item->{$key}] = $indentString . $item->{$value};
+                } else {
+                    $result[] = $indentString . $item->{$value};
+                }
+
+                // Add the children
+                /** @var \Illuminate\Database\Eloquent\Collection $childItems */
+                $childItems = $item->getChildren();
+                if ($childItems->count() > 0) {
+                    $result = $result + $buildCollection($childItems, $depth + 1);
+                }
+            }
+            return $result;
+        };
+
+        // Build a nested collection
+        $rootItems = $this->toNested();
+
+        return $buildCollection($rootItems);
+    }
+}
